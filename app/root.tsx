77eb1c41e98c15d7b71e9b39258f774287892504
sendz <sendzation@gmail.com>
2025-09-01 11:53:34 +0700

    Cleanup unused logic

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,6 @@ import {
 import type { LinksFunction } from "@remix-run/cloudflare";
 
 import "./styles.css";
-import { ThemeProvider } from "./contexts/ThemeProvider";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -31,21 +30,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        <script
-          dangerouslySetInnerHTML={{
-            __html:
-              `
-              (function() {
-                try {
-                  const theme = localStorage.getItem('theme') || 'halloween';
-                  document.documentElement.setAttribute('data-theme', theme);
-                } catch (e) {
-                  document.documentElement.setAttribute('data-theme', 'halloween');
-                }
-              })();
-            `,
-          }}
-        />
       </head>
       <body>
         {children}
@@ -58,35 +42,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return (
-     <html lang="en" data-theme="halloween">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-        <script
-          dangerouslySetInnerHTML={{
-            __html:
-              `
-              (function() {
-                try {
-                  const theme = localStorage.getItem('theme') || 'halloween';
-                  document.documentElement.setAttribute('data-theme', theme);
-                } catch (e) {
-                  document.documentElement.setAttribute('data-theme', 'halloween');
-                }
-              })();
-            `,
-          }}
-        />
-      </head>
-      <body>
-        <ThemeProvider>
-          <Outlet />
-        </ThemeProvider>
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
+    <Outlet />
+
   );
 }
