60ae2e9d99fb600e58d3b1f1d3145a2d6a11a4a8
sendz <sendzation@gmail.com>
2025-09-02 06:07:42 +0700

    Add Log

--- app/routes/statistics.tsx
+++ app/routes/statistics.tsx
@@ -1,4 +1,6 @@
-import { MetaFunction } from "@remix-run/cloudflare";
+import { json, LoaderFunctionArgs, MetaFunction } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+import { Activity } from "~/types/Activity.type";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,11 +9,30 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function loader({ context }: LoaderFunctionArgs): Promise<Array<Activity>> {
+  const { results } = await context.cloudflare.env.db.prepare(
+    `SELECT id, athlete_name, distance, moving_time, elapsed_time, total_elevation_gain, activity_date FROM activities`
+  ).all()
+
+  return results as Array<Activity>
+}
+
 const StatisticsIndex = () => {
+  const results = useLoaderData<typeof loader>();
+
+  console.log(results)
   return (
     <div className="flex flex-col min-h-screen">
       <div className="navbar bg-base-100 shadow-sm">
         <a className="btn btn-ghost text-xl" href="/statistics">Geek Bike Community Leaderboard</a>
+      </div>
+      <div>
+        {results.map(data => (
+          <div>
+            <p>Name: {data.athlete_name}</p>
+            <p>Distance: {data.distance.toLocaleString("id-ID")} meters</p>
+          </div>
+        ))}
       </div>
     </div>
   )
---
+++ app/types/Activity.type.ts
@@ -0,0 +1,9 @@
+export type Activity = {
+  id: string
+  athlete_name: string
+  distance: number
+  moving_time: number
+  elapsed_time: number
+  total_elevation_gain: number
+  activity_date: string
+}
